7183c13047e9cd5e472c66ae85eae5812df765f2
BanLowi <lowi40k@gmail.com>
2026-04-26 15:14:14 +0200

    snack 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import useSwitch from "./useSwitch";
+import useDate from "./useDate";
 
 function App() {
+  /*
+
+  SNACK 1: useSwitch
+  
   const [isOn, toggle] = useSwitch();
+  
+    return (
+      <div>
+        <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
+        <button onClick={toggle}>Cambia Stato</button>
+      </div>
+    );
+    
+    */
+
+  /* SNACK 2: useDate */
+
+  const currentDate = useDate();
 
   return (
     <div>
-      <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
-      <button onClick={toggle}>Cambia Stato</button>
+      <h1>Data e ora attuali:</h1>
+      <p>{currentDate.toLocaleString()}</p>
     </div>
   );
 }
